5db4170ba86478a0259dea8aa11531ee30a192d8
nebojsa174 <n.mihajilovic@hotmail.com>
2024-08-29 12:18:00 +0200

    initial

--- index.php
+++ index.php
@@ -10,13 +10,11 @@
 
 <body>
     <div class="h-screen flex">
-        <!-- SIDE MENU -->
-         
+        <!-- MENU -->
         <?php include './includes/menu.php'; ?>
 
-        <!-- END SIDE MENU -->
-        <!-- RIGHT -->
-        <div class="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">r</div>
+        <!-- NAVBAR -->
+        <?php include './includes/navbar.php'; ?>
     </div>
 </body>
 
