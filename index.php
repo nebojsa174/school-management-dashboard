99d81264ff5846a5990d1ca3209df8e77247c46d
nebojsa174 <n.mihajilovic@hotmail.com>
2024-08-29 12:05:38 +0200

    updated

--- index.php
+++ index.php
@@ -1,19 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="./public/css/tailwind.css">
 </head>
-<body>
-    <div class="px-5 py-5 bg-slate-500">
-        <h1 class= "font-medium">hello world</h1>
-    </div>
 
-    <a href="student">Student</a>
-    <a href="teacher">teacher</a>
-    <a href="test">test</a>
-    <a href="admin">admin</a>
+<body>
+    <div class="h-screen flex">
+        <!-- SIDE MENU -->
+         
+        <?php include './includes/menu.php'; ?>
+
+        <!-- END SIDE MENU -->
+        <!-- RIGHT -->
+        <div class="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">r</div>
+    </div>
 </body>
+
 </html>
